265fea77b56e7b7575699ebf2e5ca62e959b9eee
devfelipenunes <devfelipenunes@gmail.com>
2024-10-21 16:23:59 -0300

    feat: :sparkles: update hub

--- src/app/Hub/page.tsx
+++ src/app/Hub/page.tsx
@@ -1,12 +1,13 @@
-import EventsAndNetworking from "@/components/hub/EventsAndNetworking";
-import HeadLineHub from "@/components/hub/HeadLineHub";
-import MembershipPlans from "@/components/hub/MembershipPlans";
-import OurPartners from "@/components/hub/OurPartners";
-import Space from "@/components/hub/Space";
-import Template from "@/components/Template";
-import { FC } from "react";
+import { Template } from "@/components";
+import {
+  EventsAndNetworking,
+  HeadLineHub,
+  MembershipPlans,
+  OurPartners,
+  Space,
+} from "@/components/hub";
 
-const Hub: FC = () => {
+const Hub = () => {
   return (
     <Template>
       <HeadLineHub />
--- src/components/hub/OurPartners.tsx
+++ src/components/hub/OurPartners.tsx
@@ -15,25 +15,25 @@ const OurPartners = () => {
         </div>
         <p className="text-rich-black leading-6">
           Our strategic partnerships with the largest blockchain platforms allow
-          us to deliver cutting-edge innovaAt Bamba Labs, we believe in the
-          power of collaboration. We work hand-in-hand with industry leaders in
-          Web3, technology and hospitality to create an innovative and vibrant
-          ecosystem. Meet our strategic partners who share our vision of
-          building the future, driving growth and innovation with their unique
-          solutions.tion to startups and disruptive projects.
+          us to deliver cutting-edge innovation to startups and disruptive
+          projects. At Bamba Labs, we believe in the power of collaboration. We
+          work hand-in-hand with industry leaders in Web3, technology, and
+          hospitality to create an innovative and vibrant ecosystem. Meet our
+          strategic partners who share our vision of building the future,
+          driving growth and innovation with their unique solutions.
         </p>
       </div>
       <div className="bg-azure w-full lg:w-1/2 h-[631px] flex flex-col items-center justify-center rounded-2xl">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-4">
           {data.map((item, index) => (
             <div
               key={index}
-              className="flex justify-center bg-rich-black rounded-full transition duration-300 ease-in-out hover:bg-very-light-gray hover:scale-105"
+              className="group "
             >
               <img
                 src={item}
                 alt={`Partner ${index + 1}`}
-                className="h-auto w-full max-w-[142px] object-contain transition duration-300 ease-in-out hover:filter hover:brightness-0"
+                className={`h-auto w-full max-w-[142px] object-contain transition duration-300 ease-in-out group-hover:invert hover:scale-105`}
               />
             </div>
           ))}
