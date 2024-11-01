ac14767241e362cd8b9e08ff75e9e9235f8f9d86
devfelipenunes <devfelipenunes@gmail.com>
2024-11-01 16:57:29 -0300

    feat: :sparkles: add dark mode in events page

--- src/components/Events/HeadLineEvents.tsx
+++ src/components/Events/HeadLineEvents.tsx
@@ -2,10 +2,14 @@ const HeadLineEvents = () => {
   return (
     <div className="text-center flex flex-col items-center justify-center max-w-7xl mx-auto w-full gap-4 py-20 xl:px-0 px-6">
       <div className="flex gap-2 flex-col">
-        <p className="text-sm text-dim-gray">About Bamba Events</p>
-        <h2 className="text-rich-black text-3xl font-semibold">Bamba Events</h2>
+        <p className="text-sm text-dim-gray dark:text-[#B4B4B4]">
+          About Bamba Events
+        </p>
+        <h2 className="text-rich-black dark:text-white text-3xl font-semibold">
+          Bamba Events
+        </h2>
       </div>
-      <p className="text-dim-gray leading-6 md:w-[664px]">
+      <p className="text-dim-gray dark:text-[#B4B4B4] leading-6 md:w-[664px]">
         We proudly partner with some of the most innovative companies in the
         crypto and tech world, including Chainlink, The Graph, Arbitrum,
         Polygon, Near, Ethereum Foundation, OpenZeppelin, Ledger, Scroll,
