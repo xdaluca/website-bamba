949db3e450bb9a79d153520516825635b4f63e52
devfelipenunes <devfelipenunes@gmail.com>
2024-11-05 15:36:36 -0300

    feat: :sparkles: update contact page

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -2,6 +2,10 @@ import { Template } from "@/components";
 import FormContact from "@/components/Contact/FormContact";
 import HeadLineContact from "@/components/Contact/HeadLineContact";
 
+export const metadata = {
+  title: "Bamba Labs | Contact",
+  description: "Contact",
+};
 const Contact = () => {
   return (
     <Template>
