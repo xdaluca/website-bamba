58dcc85efdbf1fe3a6fd2032907815a4ac67400e
devfelipenunes@gmail.com <devfelipenunes@gmail.com>
2025-01-26 11:44:48 -0300

    feat: added "See Events" button to [components/OurPartners]

--- src/components/home/OurPartners.tsx
+++ src/components/home/OurPartners.tsx
@@ -43,14 +43,23 @@ const OurPartners = () => {
     <div className="max-w-7xl mx-auto flex md:flex-row flex-col items-center gap-4 py-20 px-6 xl:px-0">
       <div className="text-rich-black dark:text-[#EEEEEE] md:text-left text-center flex flex-col gap-4 py-10 px-6">
         <div className="flex flex-col gap-2">
-          <h2 className="font-semibold text-4xl">
-            Past Sponsors
-          </h2>
+          <h2 className="font-semibold text-4xl">Past Sponsors</h2>
         </div>
 
         <p className="dark:text-[#B4B4B4]">
-          Bamba's events have been sponsored by some of the leading companies in web3 and emerging technologies. 
+          Bamba's events have been sponsored by some of the leading companies in
+          web3 and emerging technologies.
         </p>
+
+        <div className="relative group w-full md:w-[166px] h-[48px] cursor-pointer hover:scale-105 transition duration-300 ease-in-out">
+          <div
+            className="absolute inset-0 rounded-full opacity-0 group-hover:opacity-100 transition-all duration-300 ease-in-out"
+            style={{ boxShadow: "0px 0px 30px 10px rgba(0, 122, 255, 0.2)" }}
+          ></div>
+          <div className="bg-rich-black dark:bg-[#EEEEEE] dark:text-[#111111] text-very-light-gray rounded-full w-full h-full flex justify-center items-center">
+            <p className="select-none">See Events</p>
+          </div>
+        </div>
       </div>
       <div className="bg-azure w-full py-10 px-6 rounded-3xl md:h-[631px] justify-center flex items-center">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
@@ -66,7 +75,7 @@ const OurPartners = () => {
                 alt={`Partner ${index + 1}`}
                 width={130}
                 height={56}
-                className="h-auto max-w-[160px] object-contain transition duration-300 ease-in-out hover:filter hover:brightness-0"
+                className="h-auto w-[200px] object-contain transition duration-300 ease-in-out hover:filter hover:brightness-0"
               />
             </Link>
           ))}
